7ef966f62b903de986da87b3fcaae4a9cbf7a4fc
Leon <92170476+L50N@users.noreply.github.com>
2023-11-04 18:24:15 +0100

    Update space-dock.sh

--- space-dock.sh
+++ space-dock.sh
@@ -43,8 +43,7 @@ display_menu() {
     echo "[4] Remove Everything (including Docker & Docker Compose)"
     echo "[5] Update Nextcloud"
     echo "[6] Exit Control Panel"
-    echo
-    read -rp "captain@bridge:~$ " choice
+    read -rp "\ncaptain@bridge:~$ " choice
 }
 
 install_nextcloud() {
